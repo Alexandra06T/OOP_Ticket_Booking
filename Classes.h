85cbef20fa473f0b187d8a108fcb5cb676cafd17
Alexandra06T <115272509+Alexandra06T@users.noreply.github.com>
2023-04-08 17:24:35 +0300

    added Piesa_teatru, Categorie, Sala, Data classes changes and adding to String and IntList classes

--- Classes.h
+++ Classes.h
@@ -11,7 +11,7 @@
 #include <iostream>
 using namespace std;
 
-// clasa utilitara MyString
+// clasa utilitara String
 class String {
     char* pointer;
     int lungime;
@@ -32,6 +32,7 @@ public:
     void add_char(char c);
     void concat(String sir);
     char* find_substr(const char* subsir);
+    friend ostream & operator << (ostream &out, String const& s);
 };
 
 String::String() {
@@ -42,6 +43,7 @@ String::String(const char *sir) {
     lungime = ::strlen(sir);
     pointer = new char[lungime];
     ::strncpy(pointer, sir, lungime);
+    pointer[lungime] = '\0';
 }
 String::~String() {
     delete[] pointer;
@@ -51,6 +53,7 @@ String::String(String &sir) {
     lungime = sir.lungime;
     pointer = new char[lungime];
     ::strncpy(pointer, sir.pointer, lungime);
+    pointer[lungime] = '\0';
 }
 void String::display() {
     int i;
@@ -58,11 +61,18 @@ void String::display() {
         cout << pointer[i];
     cout << endl;
 }
+ostream & operator << (ostream &out, String const& s) {
+    int i;
+    for (i = 0; i < s.lungime; i++)
+        out << s.pointer[i];
+    return out;
+}
 void String::add_char(char c) {
     char* newpointer = new char[lungime+1];
     ::strncpy(newpointer, pointer, lungime);
     newpointer[lungime] = c;
     lungime++;
+    newpointer[lungime] = '\0';
     pointer = newpointer;
 }
 void String::concat(const String sir) {
@@ -70,6 +80,7 @@ void String::concat(const String sir) {
     ::strncpy(newpointer, pointer, lungime);
     ::strncpy(&newpointer[lungime], sir.pointer, sir.lungime);
     lungime += sir.lungime;
+    pointer[lungime] = '\0';
     pointer = newpointer;
 }
 char* String::find_substr(const char* subsir) {
@@ -113,6 +124,7 @@ public:
     void add_last(int v);
     ~IntList();
     IntList(int dim, int *vector);
+    IntList(int dim, int k);
     IntList(IntList &lista);
     IntList& operator = (IntList const&lista){
         if (first != nullptr)
@@ -134,6 +146,8 @@ public:
     int access_val(int i);
     void set_i_val(int i, int val);
     int find_val(int val);
+    //afiseaza indicii nodurilor care au valorarea val
+    void filter(int val);
 
 };
 //crearea unei liste vide
@@ -183,6 +197,14 @@ IntList::IntList(int dim, int *vector) {
     last = first;
     for (int i = 1; i < dim; i++) {
         add_last(vector[i]);
+    }
+}
+//crearea unei liste formata din valoarea k repetata de dim ori
+IntList::IntList(int dim, int k) {
+    first  = new Node(k, nullptr);
+    last = first;
+    for (int i = 1; i < dim; i++) {
+        add_last(k);
     }
 }
 //constructor de copiere pentru o lista
@@ -263,4 +285,234 @@ int IntList::find_val(int val) {
         nod = nod->get_ptr();
     }
     return false;
+}
+void IntList::filter(int val) {
+    int indice = 1;
+    Node *nod = first;
+    while (nod != nullptr) {
+        if (nod->get_val() == val)
+            cout << indice << " ";
+        nod = nod->get_ptr();
+        indice++;
+    }
+}
+
+
+class Data {
+    int an;
+    int luna;
+    int zi;
+    int ora;
+    int minut;
+public:
+    //constructor neparametrizat
+    Data();
+    //constructor Data pentru o data calendaristica
+    Data(int o, int m, int z, int l, int a);
+    //afisare data
+    void data_display();
+    //afisare durata
+    void durata_display();
+
+};
+//constructor neparametrizat
+Data::Data(): an(2023), luna(1), zi(1), ora(0), minut(0) {};
+//constructor Data pentru o data calendaristica
+Data::Data(int o, int m, int z = 1, int l = 1, int a = 2023): zi(z), luna(l), an(a), ora(o), minut(m) {};
+
+void Data::data_display() {
+    char l[4];
+    if (luna == 1) strcpy(l,"Ian");
+    else if(luna == 2) ::strcpy(l, "Feb");
+    else if(luna == 3) ::strcpy(l, "Mar");
+    else if(luna == 4) ::strcpy(l, "Apr");
+    else if(luna == 5) ::strcpy(l, "Mai");
+    else if(luna == 6) ::strcpy(l, "Iun");
+    else if(luna == 7) ::strcpy(l, "Iul");
+    else if(luna == 8) ::strcpy(l, "Aug");
+    else if(luna == 9) ::strcpy(l, "Sep");
+    else if(luna == 10) ::strcpy(l, "Oct");
+    else if(luna == 11) ::strcpy(l, "Nov");
+    else if(luna == 12) ::strcpy(l, "Dec");
+    if(zi < 10) cout << 0;
+    cout << zi << " " << l << " " << an << " ";
+    if(ora < 10) cout << 0;
+    cout << ora << ":";
+    if(minut < 10) cout << 0;
+    cout << minut << endl;
+}
+//afisare durata
+void Data::durata_display() {
+    if( ora == 0 && minut == 0)
+        cout << "-\n";
+    else
+        cout << ora << " h " << minut << " min\n";
+}
+
+class Sala {
+    String nume_sala;
+    String adresa;
+    int nr_locuri_C1;
+    int nr_locuri_C2;
+    int nr_locuri_C3;
+public:
+    //constructor parametrizat Sala
+    Sala(const char* nume, const char* adr, int nr_C1, int nr_C2, int nr_C3);
+    //afiseaza informatii despre nume_sala si adresa
+    void display();
+    //getter nr_locuri_C1
+    int get_C1() const;
+    //getter nr_locuri_C2
+    int get_C2() const;
+    //getter nr_locuri_C3
+    int get_C3() const;
+    //setter nume_sala
+    void set_nume(const char* nume);
+    //setter adresa
+    void set_adresa(const char* adr);
+    //setter nr_locuri_C1
+    void set_C1(int nr_C1);
+    //setter nr_locuri_C2
+    void set_C2(int nr_C2);
+    //setter nr_locuri_C3
+    void set_C3(int nr_C3);
+
+};
+//constructor parametrizat Sala
+Sala::Sala(const char* nume, const char* adr, int nr_C1, int nr_C2, int nr_C3): nume_sala(nume), adresa(adr), nr_locuri_C1(nr_C1), nr_locuri_C2(nr_C2), nr_locuri_C3(nr_C3)  {}
+
+//afiseaza informatii despre nume_sala si adresa
+void Sala::display() {
+    nume_sala.display();
+    adresa.display();
+}
+//getter nr_locuri_C1
+int Sala::get_C1() const {
+    return nr_locuri_C1;
+}
+//getter nr_locuri_C2
+int Sala::get_C2()  const {
+    return nr_locuri_C2;
+}
+//getter nr_locuri_C3
+int Sala::get_C3() const {
+    return nr_locuri_C3;
+}
+//setter nume_sala
+void Sala::set_nume(const char* nume) {
+    nume_sala = nume;
+}
+//setter adresa
+void Sala::set_adresa(const char* adr) {
+    adresa = adr;
+}
+//setter nr_locuri_C1
+void Sala::set_C1(int nr_C1) {
+    nr_locuri_C1 = nr_C1;
+}
+//setter nr_locuri_C2
+void Sala::set_C2(int nr_C2) {
+    nr_locuri_C2 = nr_C2;
+}
+//setter nr_locuri_C3
+void Sala::set_C3(int nr_C3) {
+    nr_locuri_C3 = nr_C3;
+}
+
+class Piesa_teatru {
+    String nume_piesa;
+    String autor;
+    String regie;
+    String decor;
+    String costume;
+    String muzica;
+    Data durata;
+    Data pauza;
+public:
+    //constructor Piesa_teatru parametrizat
+    Piesa_teatru(String nume, String a, String r, String d, String c, String m, int od, int md, int op, int mp);
+    //afisare informatii despre o piesa de taetru
+    void piesa_display();
+
+};
+//constructor Piesa_teatru parametrizat
+Piesa_teatru::Piesa_teatru(String nume, String a, String r, String d, String c,
+                           String m, int od, int md, int op = 0, int mp = 0):
+                           durata(od, md),
+                           nume_piesa(nume),
+                           autor(a),
+                           regie(r),
+                           decor(d),
+                           costume(c),
+                           muzica(m),
+                           pauza(op, mp) {};
+//afisare informatii despre o piesa de teatru
+void Piesa_teatru::piesa_display() {
+    cout << nume_piesa << "\nde " << autor << endl;
+    cout << "Regie: " << regie << endl;
+    cout << "Decor: " << decor << endl;
+    cout << "Costume: " << costume << endl;
+    cout << "Muzica: " << muzica << endl;
+    cout << "Durata: "; durata.durata_display();
+    cout << "Pauza: "; pauza.durata_display();
+}
+class Categorie {
+    //pentru fiecare categorie in parte, locurile sunt etichetate de la 1 la nr de locuri total din categorie
+    float pret;
+    int nr_locuri_disponibile;
+    int nr_locuri_rezervate;
+    //lista alocata dinamic ce retine pt fiecare loc in parte daca este disponibil(0) sau ocupat(1)
+    IntList lista_locuri;
+public:
+    //constructor neparametrizat Categorie
+    Categorie();
+    //constructor neparametrizat Categorie
+    Categorie(float p, int d);
+    //getter pret
+    float get_pret();
+    //setter pret
+    void set_pret(float pret_nou);
+    //setter loc devine rezervat
+    void set_rezervat(int i);
+    //setter loc devine disponibil
+    void set_disponibil(int i);
+    //afiseaza lista locuri disponibile
+    void display_disponibil();
+    //calculeaza pret, aplicand eventuale reduceri (0% (cod reducere 0), 20% ultima zi (cod reducere 1), 50% student (cod reducere 2), 100% elevi (cod reducere 3)
+    float pret_final(int statut);
+
+};
+//constructor neparametrizat Categorie
+Categorie::Categorie(): pret(0), nr_locuri_rezervate(0), nr_locuri_disponibile(0), lista_locuri() {};
+//constructor parametrizat Categorie: la crearea unei categorii, toate locurile sunt disponibile
+Categorie::Categorie(float p, int d): pret(p), nr_locuri_disponibile(d), nr_locuri_rezervate(0), lista_locuri(d, 0)  {};
+
+//getter pret
+float Categorie::get_pret() {
+    return pret;
+}
+//setter pret
+void Categorie::set_pret(const float pret_nou) {
+    pret = pret_nou;
+}
+//setter loc devine rezervat
+void Categorie::set_rezervat(const int i) {
+    lista_locuri.set_i_val(i, 1);
+}
+//setter loc devine disponibil
+void Categorie::set_disponibil(int i) {
+    lista_locuri.set_i_val(i,0);
+}
+//afiseaza lista locuri disponibile
+void Categorie::display_disponibil() {
+    lista_locuri.filter(0);
+}
+float Categorie::pret_final(int cod) {
+    if(cod == 1)
+        return pret/5;
+    else if(cod ==2)
+        return pret/2;
+    else if(cod == 3)
+        return 0;
+    return pret;
 }
--- main.cpp
+++ main.cpp
@@ -3,17 +3,38 @@
 
 int main() {
 
-    int v[5] = {1, 2, 3, 4,5 };
-    IntList li(5, v), l2(li), l3 = li, l4;
-    l4.concat(l2,l3);
-    l2.display();
-    l3.display();
-    l4.display();
-    l2.display();
-    l3.display();
-    cout << li.access_val(1) << endl;
-    l2.set_i_val(5, 7);
-    l2.display();
-    cout << l2.find_val(3);
+//    int v[5] = {1, 2, 3, 4,5 };
+//    IntList li(5, v), l2(li), l3 = li, l4;
+//    l4.concat(l2,l3);
+//    l2.display();
+//    l3.display();
+//    l4.display();
+//    l2.display();
+//    l3.display();
+//    cout << li.access_val(1) << endl;
+//    l2.set_i_val(5, 7);
+//    l2.display();
+//    cout << l2.find_val(3);
+//    Sala A("Aaa", "Str. nr.", 15, 20, 40);
+//    A.display();
+//    cout << A.get_C1() << endl;
+//    A.set_C1(1);
+//    A.set_nume("Bbb");
+//    A.display();
+//    cout << A.get_C1() << endl;
+//    cout << ::strlen("ana");
+//    String s("Ana"), t("bbb");
+//    cout << s;
+//    s.concat(t);
+//    s.display();
+    Piesa_teatru p("Cei drepti", "Albert Camus", "Mihai Maniutiu", "Adrian Damian", "Luiza Enescu", "Mihai Dobre", 1, 30);
+    p.piesa_display();
+    Data d;
+    d.data_display();
+    Categorie c(50, 20);
+    c.set_rezervat(3);
+    c.display_disponibil();
+    cout << endl << c.pret_final(0);
     return 0;
 }
+
